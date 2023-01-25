5dfeca86a7648a7db55e169bf501f3aaa692935e
Gustavo Souza <gustavopsouza33@gmail.com>
2023-01-25 15:52:22 -0300

    Script para baixar arquivos de todos os workspaces do Power BI

---
+++ baixar power bi.ps1
@@ -0,0 +1,26 @@
+﻿Connect-PowerBIServiceAccount
+
+ 
+$ws = Get-PowerBIWorkspace
+$caminho = "C:\powerbi\"
+
+
+foreach ($pasta in $ws) {
+  $work = $pasta.Name
+  $nome = $caminho + $work
+  New-Item -Path $nome -ItemType Directory
+}
+foreach ($w in $ws) {
+  $work = $w.Name
+  $rel = Get-PowerBIWorkspace -Name $work
+  $rel2 = Get-PowerBIReport -Workspace $rel
+  foreach ($r in $rel2) {
+    $arquivo = $caminho + $work + "\" + $r.Name + ".pbix"
+
+    Export-PowerBIReport -Id $r.Id -OutFile $arquivo
+
+    Write-Output $arquivo "Salvo"
+
+
+  }
+}
